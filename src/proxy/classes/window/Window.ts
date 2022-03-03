a01215bc056b35af7e0c222d65c82f7e8fad4d79
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-03-03 17:29:52 +0100

    created Window.ts

---
+++ src/proxy/classes/window/Window.ts
@@ -0,0 +1,57 @@
+import EventEmitter from "events"
+import { Item } from "./Item"
+import {windowClickEvent} from "../../interfaces/windowClickEvent";
+
+/**
+ * Class to represent minecraft container windows
+ */
+export class Window extends EventEmitter {
+
+    items: Item[] = []
+
+    /**
+     * Constructor of the Window class
+     * @param id - Window ID
+     * @param type - Window type
+     * @param title - Window title
+     * @param slotCount - Slots in the window
+     */
+    constructor(public id: number, public type: string, public title: string, public slotCount: number) {
+        super()
+    }
+
+    /**
+     * Add an item the window's item array (doesn't send packet!)
+     * @param item - Item instance
+     */
+    addItem(item: Item) {
+        this.items.push(item)
+    }
+
+    /**
+     * Add an array of items the window's item array (doesn't send packet!)
+     * @param items - Array of Item instances
+     */
+    addItems(items: Item[]) {
+        this.items.push(...items)
+    }
+
+    /**
+     * Generate the JSON for you to send with the "window_items" packet
+     */
+    genItemJSON() {
+        const it = []
+        for (const item of this.items) {
+            it.push(item.genJSON())
+        }
+        return it
+    }
+
+    /**
+     * Call this when the window is clicked on
+     * @param event
+     */
+    onClick(event: windowClickEvent) {
+        this.emit("click", event)
+    }
+}
