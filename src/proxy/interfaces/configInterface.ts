9cacf49277bc1077a3f0027aa4196c9d7273088e
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-03-03 16:26:11 +0100

    created configInterface.ts

---
+++ src/proxy/interfaces/configInterface.ts
@@ -0,0 +1,20 @@
+export interface configInterface {
+    account: {
+        email: string,
+        password: string,
+        auth: "mojang" | "microsoft",
+        hypixelApiKey: string
+    },
+
+    packet: {
+        enableMods: boolean,
+        particles: boolean
+    },
+
+    modules: {
+        packetFilter: boolean,
+        playerModule: boolean,
+        autoDodge: boolean,
+        streamMod: boolean
+    }
+}
