cbf299ff52212815c81d8076353903fb3bc16d15
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-03-03 17:07:56 +0100

    created Item.ts to hold mc items

---
+++ src/proxy/classes/window/Item.ts
@@ -0,0 +1,42 @@
+/**
+ * Very simple representation of an MC item
+ */
+export class Item {
+    /**
+     * Constructor of the Item class
+     * @param id - ID
+     * @param name - Display Name
+     * @param dmg - Item Damage (for different colored glass etc.)
+     */
+    constructor(public id: number, public name: string, public dmg: number | undefined) {}
+
+    /**
+     * Generates the JSON for you to send with the "window_items" packet
+     */
+    genJSON() {
+        return {
+            blockId: this.id,
+            itemCount: 1,
+            itemDamage: this.dmg,
+            nbtData: {
+                "type": "compound",
+                "name": "",
+                "value": {
+                    "overrideMeta": {
+                        "type": "byte",
+                        "value": 1
+                    },
+                    "display": {
+                        "type": "compound",
+                        "value": {
+                            "Name": {
+                                "type": "string",
+                                "value": this.name
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
