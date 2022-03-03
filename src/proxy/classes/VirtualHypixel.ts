ad5e2e16241b4dc021259f515e9bbedc9d391208
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-03-03 16:38:07 +0100

    VirtualHypixel.ts now creates proxy

--- src/proxy/classes/VirtualHypixel.ts
+++ src/proxy/classes/VirtualHypixel.ts
@@ -1,6 +1,7 @@
 import { configInterface } from "../interfaces/configInterface"
 import { InstantConnectProxy } from "prismarine-proxy"
 import { Client } from "minecraft-protocol"
+import { logger } from "../../utils/logger"
 
 /**
  * Main Virtual Hypixel Class
@@ -15,6 +16,39 @@ export class VirtualHypixel {
     client: Client | undefined
 
     constructor(public config: configInterface) {
+        logger.info(`Starting Virtual Hypixel ${this.version}...`)
+
+        this.proxy =  new InstantConnectProxy({
+            loginHandler: (client) => {
+                this.client = client
+
+                logger.info(`Logging in as ${client.profile.name}...`)
+
+                return { username: config.account.email, password: config.account.password, auth: config.account.auth }
+            },
+            serverOptions: {
+                version: "1.8.9",
+                motd: `Virtual Hypixel Server`,
+                maxPlayers: 69,
+                //favicon: fs.readFileSync("./data/favicon.txt").toString()
+            },
+            clientOptions: {
+                version: "1.8.9",
+                host: "hypixel.net"
+            }
+        })
+
+        // @ts-ignore
+        this.proxy.on('incoming', (data, meta, toClient, toServer) => {
+            toClient.write(meta.name, data)
+        })
+
+        // @ts-ignore
+        this.proxy.on('outgoing', (data, meta, toClient, toServer) => {
+            toServer.write(meta.name, data)
+        })
+
+        logger.info(`Ready! Connect to "localhost" to start playing!`)
     }
 
 }
