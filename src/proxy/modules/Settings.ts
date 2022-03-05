64d66b4538244bd43b6b3f0b11a912a6549a3817
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-03-05 17:51:28 +0100

    switch to VirtualHypixel.ts windowManager

--- src/proxy/modules/Settings.ts
+++ src/proxy/modules/Settings.ts
@@ -2,6 +2,8 @@ import { WindowManager } from "../classes/window/WindowManager"
 import { VirtualHypixel } from "../classes/VirtualHypixel"
 import { Client, PacketMeta } from "minecraft-protocol"
 import { ModuleBase } from "./base/ModuleBase"
+import { logger } from "../../utils/logger"
+import {Item} from "../classes/window/Item";
 
 /**
  * Settings module
@@ -10,16 +12,16 @@ import { ModuleBase } from "./base/ModuleBase"
 export class Settings extends ModuleBase {
 
     windowId: number = 69 // window id to use
-    windowManager: WindowManager = new WindowManager()
 
     constructor(client: Client, virtual: VirtualHypixel) {
         super("Settings", "1.0.0", client, virtual);
     }
 
     openWindow() {
-        this.windowManager.createWindow(this.client, this.windowId, "Virtual Hypixel", 9, () => {
-            console.log("CLICK")
+        this.virtual.windowManager.createWindow(this.client, this.windowId, "Virtual Hypixel", 9, () => {
+            logger.debug("click!")
         })
+        this.virtual.windowManager.addItems(this.client, this.windowId, [new Item(160, "test", 0)])
     }
 
     onOutPacket(meta: PacketMeta, data: any, toServer: Client): [boolean, any] {
