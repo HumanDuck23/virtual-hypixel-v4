f1e07ca5bef7be2404f6f3926f2f7c787700ea41
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-03-05 10:16:16 +0100

    allowed modules to filter / modify packets

--- src/proxy/classes/VirtualHypixel.ts
+++ src/proxy/classes/VirtualHypixel.ts
@@ -2,8 +2,9 @@ import { configInterface } from "../interfaces/configInterface"
 import { ModuleBase } from "../modules/base/ModuleBase"
 import { InstantConnectProxy } from "prismarine-proxy"
 import { WindowManager } from "./window/WindowManager"
-import { Client } from "minecraft-protocol"
+import {Client, PacketMeta} from "minecraft-protocol"
 import { logger } from "../../utils/logger"
+import {PacketFilter} from "../modules/PacketFilter";
 
 /**
  * Main Virtual Hypixel Class
@@ -30,6 +31,8 @@ export class VirtualHypixel {
                 logger.info(`Logging in as ${client.profile.name}...`)
                 logger.info(`Loading modules...`)
 
+                this.modules.push(new PacketFilter(this.client, this))
+
                 return { username: config.account.email, password: config.account.password, auth: config.account.auth }
             },
             serverOptions: {
@@ -46,20 +49,41 @@ export class VirtualHypixel {
 
         // @ts-ignore
         this.proxy.on('incoming', (data, meta, toClient, toServer) => {
-            toClient.write(meta.name, data)
+            const handled = this.handlePacket(meta, data, toServer, false)
+            if (!handled.intercept)
+                toClient.write(handled.meta.name, handled.data)
         })
 
         // @ts-ignore
         this.proxy.on('outgoing', (data, meta, toClient, toServer) => {
-            let intercept = false
-            intercept = this.windowManager.onOutPacket(meta, data, toServer)
+            let winIntercept = this.windowManager.onOutPacket(meta, data, toServer)
 
+            const handled = this.handlePacket(meta, data, toServer, true)
 
-            if (!intercept)
-                toServer.write(meta.name, data)
+            if (!winIntercept && !handled.intercept)
+                toServer.write(handled.meta.name, handled.data)
         })
 
         logger.info(`Ready! Connect to "localhost" to start playing!`)
+    }
+
+    handlePacket(meta: PacketMeta, data: any, toServer: Client, out: boolean): { intercept: boolean, meta: PacketMeta, data: any } {
+        let intercept = false
+        for (const module of this.modules) {
+            let applied
+            if (out)
+                applied = module.onOutPacket(meta, data, toServer)
+            else
+                applied = module.onInPacket(meta, data, toServer)
+
+            if (applied[0]) {
+                intercept = true
+            } else {
+                data = applied[1]
+            }
+        }
+
+        return { intercept, meta, data }
     }
 
 }
