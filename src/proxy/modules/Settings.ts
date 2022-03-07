b3ccd7af8d0672a9399ad2775c461bf2a9ab8432
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-03-07 16:36:50 +0100

    created Settings.ts for settings window, dummy items

--- src/proxy/modules/Settings.ts
+++ src/proxy/modules/Settings.ts
@@ -1,8 +1,10 @@
+import { CustomSkull } from "../classes/window/item/CustomSkull"
 import { VirtualHypixel } from "../classes/VirtualHypixel"
 import { Client, PacketMeta } from "minecraft-protocol"
 import { ModuleBase } from "./base/ModuleBase"
-import { Item } from "../classes/window/Item"
+import { Item } from "../classes/window/item/Item"
 import { logger } from "../../utils/logger"
+import { utils } from "../../utils/utils"
 
 /**
  * Settings module
@@ -11,16 +13,54 @@ import { logger } from "../../utils/logger"
 export class Settings extends ModuleBase {
 
     windowId: number = 69 // window id to use
+    loadedLayout: boolean = false
+
+    layout: Item[] = [
+        ...utils.repeatObj<Item>(new Item(160, "ee", 15), 8),
+        new CustomSkull("TEST", "f081021a-a454-4f15-bf2d-af9d9935e1f2")
+    ]
 
     constructor(client: Client, virtual: VirtualHypixel) {
         super("Settings", "1.0.0", client, virtual);
     }
 
     openWindow() {
-        this.virtual.windowManager.createWindow(this.client, this.windowId, "Virtual Hypixel", 9, () => {
-            logger.debug("click!")
+        if (!this.loadedLayout) {
+            this.loadLayout()
+                .then(() => {
+                    this.openWindow()
+                })
+        } else {
+            this.virtual.windowManager.createWindow(this.client, this.windowId, "Virtual Hypixel", 9, () => {
+                logger.debug("click!")
+            })
+            this.virtual.windowManager.addItems(this.client, this.windowId, this.layout)
+        }
+    }
+
+    loadLayout() {
+        return new Promise(async (resolve, reject) => {
+            let skullCount = 0
+            let loadedCount = 0
+
+            for (const item of this.layout) {
+                if (item instanceof CustomSkull) skullCount++
+            }
+
+            logger.debug(`Loading ${skullCount} skulls.`)
+
+            for (const item of this.layout) {
+                if (item instanceof CustomSkull) {
+                    await item.loadData().catch(e => { logger.error(`Error in skull: ${e}`) })
+                    loadedCount++
+                    if (loadedCount === skullCount) {
+                        this.loadedLayout = true
+                        resolve(1)
+                    }
+                }
+            }
         })
-        this.virtual.windowManager.addItems(this.client, this.windowId, [new Item(160, "test", 0)])
+
     }
 
     onOutPacket(meta: PacketMeta, data: any, toServer: Client): [boolean, any] {
